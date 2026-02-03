81f8d5579a0ff0c8e95c3a97d1fa94a9940cb2a0
Renata <khotiakovarena@gmail.com>
2026-02-03 18:41:55 +0200

    fixed mistakes syntax

--- src/types/ErrorMessage.ts
+++ src/types/ErrorMessage.ts
@@ -1,7 +1,7 @@
 export enum ErrorMessage {
   LoadTodos = 'Unable to load todos',
-  AddTodo = 'Unable to add todo',
-  DeleteTodo = 'Unable to delete todo',
-  UpdateTodo = 'Unable to update todo',
+  AddTodo = 'Unable to add a todo',
+  DeleteTodo = 'Unable to delete a todo',
+  UpdateTodo = 'Unable to update a todo',
   TitleValue = 'Title should not be empty',
 }
